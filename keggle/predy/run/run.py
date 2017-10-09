817f0ef9d7fd0f2a4820baa3c054dd967b210ba1
ZhartunMatthew <zhartunmatthew@gmail.com>
2017-10-09 11:26:14 +0300

    Switched to xgboost

--- keggle/predy/run/run.py
+++ keggle/predy/run/run.py
@@ -1,7 +1,8 @@
 from time import time
-from sklearn.decomposition import PCA
 from sklearn.ensemble import GradientBoostingClassifier as Model
 import numpy as np
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
 
 
 def load_data(path, train=True):
@@ -29,24 +30,18 @@ def create_submission(labels, answers, file_name):
     return
 
 
-def calculate_gini(y, pred):
-    g = np.asarray(np.c_[y, pred, np.arange(len(y))])
-    g = g[np.lexsort((g[:, 2], -1*g[:, 1]))]
-    gs = g[:, 0].cumsum().sum() / g[:, 0].sum()
-    gs -= (len(y) + 1) / 2.0
-    return gs / len(y)
+def gini(actual, predicted):
+    elements = np.asarray(np.c_[actual, predicted, np.arange(len(actual))], dtype=np.float)
+    elements = elements[np.lexsort((elements[:, 2], -1*elements[:, 1]))]
+    total_losses = elements[:, 0].sum()
+    gini_sum = elements[:, 0].cumsum().sum() / total_losses
+    gini_sum -= (len(actual) + 1) / 2.
+    return gini_sum / len(actual)
 
 
-def norm_gini(predicted, expected):
-    return calculate_gini(expected, predicted) / calculate_gini(expected, expected)
-
-
-def compute_sparse(data):
-    result = np.zeros((len(data[0])), float)
-    for row in data:
-        result += np.asarray(np.array(row == -1), int)
-
-    return np.array((result / len(data)) * 100, int)
+def gini_xgb(predicted, expected):
+    expected = expected.get_label()
+    return 'gini', gini(expected, predicted) / gini(expected, expected)
 
 
 class Estimator(object):
@@ -66,60 +61,52 @@ class Estimator(object):
         return self.model.__str__().split('(')[0]
 
 
-def add_extra_columns_to_train(train_data, addition):
-    for i in range(len(addition[0])):
-        for j in range(len(addition[0])):
-            temp = addition[:, i] * addition[:, j]
-            train_data = np.insert(train_data, 0, temp, 1)
+class XEstimator(object):
 
-    return train_data
+    def __init__(self):
+        self.params = {
+                       'eta': 0.02, 'max_depth': 4,
+                       'subsample': 0.9, 'colsample_bytree': 0.9,
+                       'objective': 'binary:logistic',
+                       'eval_metric': 'auc', 'seed': 99,
+                       'silent': True}
 
+        self.model = None
 
-def add_extra_columns_to_test(test_data, addition):
-    for i in range(len(addition[0])):
-        for j in range(len(addition[0])):
-            temp = addition[:, i] * addition[:, j]
-            test_data = np.insert(test_data, 1, temp, 1)
+    def fit(self, data):
+        x = data[:, :-1]
+        y = data[:,  -1]
 
-    return test_data
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=99)
+        self.model = xgb.train(self.params,
+                               xgb.DMatrix(x, label=y),
+                               5000,
+                               [(xgb.DMatrix(x_train, y_train), 'train'), (xgb.DMatrix(x_test, y_test), 'valid')],
+                               feval=gini_xgb,
+                               maximize=True,
+                               verbose_eval=10)
+
+    def predict(self, data):
+        return self.model.predict(xgb.DMatrix(data[:, 1:]))
+
+    def name(self):
+        return self.model.__str__().split('(')[0]
 
 
 def run(train_data, test_data, estimator):
     start = time()
     estimator.fit(train_data)
-    test_ans = estimator.predict(train_data)
 
-    # gini computing for training_set
-    gini = norm_gini(test_ans, train_data[:, -1])
-    print('Training data gini coefficient: %f' % gini)
-    # 3 add -
-    # 0 add -
-    # 0 add (exp) -
-
-    # test data submission
     submission_answers = estimator.predict(test_data)
-    create_submission(test_data[:, 0], submission_answers, estimator.name() + str(time()))
+    create_submission(test_data[:, 0], submission_answers, 'submission')
     print('Submission took: %.3f sec.' % (time() - start))
 
 
-def main(comp):
+def main():
     train_data = load_data('../../data/train.csv')
     test_data = load_data('../../data/test.csv', False)
-
-    pca_train = PCA(n_components=comp)
-    pca_train.fit(train_data[:, :-1])
-    imp_train_data = pca_train.transform(train_data[:, :-1])
-    train_data = add_extra_columns_to_train(train_data, imp_train_data)
-
-    pca_test = PCA(n_components=comp)
-    pca_test.fit(test_data[:, 1:])
-    imp_test_data = pca_test.transform(test_data[:, 1:])
-    test_data = add_extra_columns_to_test(test_data, imp_test_data)
-
-    run(train_data, test_data, Estimator(Model(verbose=True)))
+    run(train_data, test_data, XEstimator())
 
 
 if __name__ == '__main__':
-    main(0)
-    main(3)
-    main(10)
+    main()
